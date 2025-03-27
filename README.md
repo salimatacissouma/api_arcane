7201ddf1dc143a2cc39fb624cfd25e96a8dd62d9
salimatacissouma <salimatacissouma23@gmail.com>
2025-03-27 13:18:06 +0100

    Implementing the model and creating entities in the database

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # api_arcane
+Etape1: Création de la base de données
+    sudo -u postgres psql
+    CREATE DATABASE immobilier;
+
+Etape2: Création des tables et association dans la base de données.
--- app.py
+++ app.py
@@ -1,7 +1,58 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# Configuration de la base de données
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/immobilier'
+
+# Initialisation de SQLAlchemy
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    lastname = db.Column(db.String(100), nullable=False)
+    firstname = db.Column(db.String(100), nullable=False)
+    date_of_birth = db.Column(db.Date, nullable=False)
+    email = db.Column(db.String(200), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    is_owner = db.Column(db.Boolean, default=False)
+    createdAt = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updatedAt = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+    properties = db.relationship('Property', backref='owner', lazy=True)
+
+
+class Property(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    type = db.Column(db.String(100), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+    createdAt = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updatedAt = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    pieces = db.relationship('Piece', backref='property', lazy=True)
+
+
+class Piece(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    createdAt = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updatedAt = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+    property_id = db.Column(db.Integer, db.ForeignKey('property.id'), nullable=False)
+
+# Réinitialisé la base de données
+def reset_database():
+    with app.app_context():
+        db.drop_all()
+        db.session.commit()  # S'assurer que la suppression est bien prise en compte
+        db.create_all()
+        print("Base de données réinitialisée avec succès !")
+
+# Exécution principale
+if __name__ == '__main__':
+    reset_database()
